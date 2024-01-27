ea653c2f6c4adc35974540e492ab2256e7d657fe
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2024-01-27 22:41:03 +0530

    ui fixes

--- src/components/ComingSoon/ComingSoon.jsx
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -7,7 +7,7 @@ export default function ComingSoon() {
     return (
         <div className="fullscreen-video-container">
               <div className="coming-soon-text-overlay">
-                  <h1>COMING SOON...</h1>
+                  <h1 className='coming-soon-text-h1'>COMING SOON...</h1>
               </div>
               <div className="coming-soon-bg"></div>
 
