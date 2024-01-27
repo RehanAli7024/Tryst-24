48703941114791c52282a9bf46a98a332413d43d
Sudhanshu Kumar <97729053+sudhanshusmith@users.noreply.github.com>
2024-01-27 17:52:32 +0530

    mapped the data in contact us

--- src/pages/contactus/data.js
+++ src/pages/contactus/data.js
@@ -1,2 +1,47 @@
-const data = [];
+import oc1 from "../../assets/oc1.png";
+import oc2 from "../../assets/oc2.png";
+
+const data = [{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc1,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc2,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc2,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc2,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc2,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+{
+  name: 'Harshit Mawandia',
+  por: 'Overall Coordinator',
+  image: oc2,
+  email: "mailto:harshitm@gmail.com",
+  linkedin: "https://www.linkedin.com/in/harshit-mawandia-8a7b3b1b3/",
+},
+];
 export default data;
