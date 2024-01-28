f6024f47e199fdd2bdebd3065bd7ee4eb2c44e2c
Shivannsh <bb1221392@iitd.ac.in>
2024-01-28 17:41:10 +0530

    Navbar Fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import trystlogo from "../../assets/Navbar/TrystLogo.png";
 import Tryst24 from "../../assets/Navbar/TRYST.png";
 import profileicon from "../../assets/Navbar/IconButton.png";
 import navbarmenu from "../../assets/Navbar/navbarmenu.png";
-import crossmenu from '../../assets/Navbar/crossmenu.png';
+import crossmenu from "../../assets/Navbar/crossmenu.png";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 function Navbar() {
   const navigate = useNavigate();
   const [showNavOptions, setShowNavOptions] = useState(false);
   const [selectedOption, setSelectedOption] = useState(""); // Default selected option
-  const [selectedMobileOption, setSelectedMobileOption] = useState(selectedOption); // Mobile view selected option
+  const [selectedMobileOption, setSelectedMobileOption] =
+    useState(selectedOption); // Mobile view selected option
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
 
   const handleShowNavbar = () => {
@@ -41,42 +42,76 @@ function Navbar() {
   }, []);
 
   return (
-    <Container className={`navbar ${isNavbarVisible ? "navbar-visible" : "navbar-hidden"}`} >
-      <div className="navbarheader" >
-        <div  className="navbartrystlogo" onClick={() => handleNavbarOptionClick("")} >
+    <Container
+      className={`navbar ${
+        isNavbarVisible ? "navbar-visible" : "navbar-hidden"
+      }`}
+    >
+      <div className="navbarheader">
+        <div
+          className="navbartrystlogo"
+          onClick={() => handleNavbarOptionClick("")}
+        >
           <img className="trystlogoimg" src={trystlogo}></img>
           <img className="tryst2024img" src={Tryst24}></img>
         </div>
-        <div className="navbaricons">
-          <a
-            className={`navbaroption ${selectedOption === 'CAP' ? "navbaroption-selected" : ""}`}
-            href="https://cap.tryst-iitd.org/" target="_blank" rel="noreferrer"
-          >
-            CAP
-          </a>
-          {["About", "Guests", "Pronites", "Events", "Sponsors", "Contact Us"].map((option) => (
-            <div
-              key={option}
-              className={`navbaroption ${selectedOption === option ? "navbaroption-selected" : ""}`}
-              onClick={() => handleNavbarOptionClick(option)}
+        <div className="navbaroptions-container">
+          <div className="navbaricons">
+            <a
+              className={`navbaroption ${
+                selectedOption === "CAP" ? "navbaroption-selected" : ""
+              }`}
+              href="https://cap.tryst-iitd.org/"
+              target="_blank"
+              rel="noreferrer"
             >
-              {option}
-            </div>
-          ))}
+              CAP
+            </a>
+            {[
+              "About",
+              "Guests",
+              "Pronites",
+              "Events",
+              "Sponsors",
+              "Contact Us",
+            ].map((option) => (
+              <div
+                key={option}
+                className={`navbaroption ${
+                  selectedOption === option ? "navbaroption-selected" : ""
+                }`}
+                onClick={() => handleNavbarOptionClick(option)}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+          <div className="navbarprofile">
+            <img src={profileicon}></img>
+          </div>
+          <button className="navbarmenu" onClick={handleShowNavbar}>
+            <img src={showNavOptions ? crossmenu : navbarmenu}></img>
+          </button>
         </div>
-        <div className="navbarprofile">
-          <img src={profileicon}></img>
-        </div>
-        <button className="navbarmenu" onClick={handleShowNavbar}>
-
-          <img src={showNavOptions ? crossmenu : navbarmenu}></img>
-        </button>
       </div>
-      <div className={showNavOptions ? 'navbariconsmobile' : 'hiddenmobiletoggle'}>
-        {["About", "Guests", "Pronites", "Events", "Sponsors", "Contact Us"].map((option) => (
+      <div
+        className={showNavOptions ? "navbariconsmobile" : "hiddenmobiletoggle"}
+      >
+        {[
+          "About",
+          "Guests",
+          "Pronites",
+          "Events",
+          "Sponsors",
+          "Contact Us",
+        ].map((option) => (
           <div
             key={option}
-            className={`navbaroption ${selectedMobileOption === option ? "navbaroption-selected-mobile" : ""}`}
+            className={`navbaroption ${
+              selectedMobileOption === option
+                ? "navbaroption-selected-mobile"
+                : ""
+            }`}
             onClick={() => handleNavbarOptionClick(option)}
           >
             {option}
@@ -87,7 +122,6 @@ function Navbar() {
   );
 }
 export default Navbar;
-
 
 const Container = styled.div`
   display: flex;
