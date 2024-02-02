3bfdb76786b810ccbb58e5ab382c6288cd2c0d5a
Rajarshee Das <rajarsheedas14@gmail.com>
2024-02-02 19:37:46 +0530

    Event Page User Info Done

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -2,12 +2,17 @@ import "./dashboard.css";
 import photo from "../Events/poster.webp";
 import react, { useState } from "react";
 import edit_button from "./btn.png";
+import logoutbutton from "./Button.png";
 
-import UserCard from "../../components/userCard/UserCard_Registration"
+import UserCard from "../../components/userCard/UserCard_Registration";
 
 const Dashboard = () => {
-
-
+  const logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    localStorage.removeItem("user");
+    navigate("/");
+  };
   return (
     <>
       <div className="dashboard">
@@ -19,7 +24,41 @@ const Dashboard = () => {
               <img src={edit_button} className="edit_photo" alt="" />
             </button>
           </div>
-          <div className="profile_details">Rajarshee</div>
+          <div className="profile_details">
+            <div className="user_name_logout">
+              <div className="user_name">Rajarshee</div>
+              <button className="logout_btn" onClick={logout}>
+                <img src={logoutbutton} alt="" />
+              </button>
+            </div>
+            <div className="email_phone">
+              <div className="email_label">E-MAIL:</div>
+              <div className="user_email">rajarsheedas14@gmail.com</div>
+              <div className="phone_label">PHONE:</div>
+              <div className="user_email">9485074550</div>
+            </div>
+            <div className="user_ID">
+              <div className="email_label">UID:</div>
+              <div className="user_email">Raj8976</div>
+            </div>
+            <div className="college_details">
+              <div className="email_label">college details :</div>
+              <div className="actual_details">
+                <div className="detail_info">
+                  <div className="info_label">Name:</div>
+                  <div className="info_data">IIT Delhi</div>
+                </div>
+                <div className="detail_info">
+                  <div className="info_label">State:</div>
+                  <div className="info_data">Delhi</div>
+                </div>
+                <div className="detail_info">
+                  <div className="info_label">City:</div>
+                  <div className="info_data">New Delhi</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="dashboard-nav">
@@ -28,7 +67,7 @@ const Dashboard = () => {
           <button className="dashboard-nav-button">YOUR ORDERS</button>
           <button className="dashboard-nav-button">ACCOMODATION</button>
         </div>
-        <div className="dashboard-nav-details">
+        {/* <div className="dashboard-nav-details">
           <UserCard className="dashboard-nav-details-card" />
           <UserCard className="dashboard-nav-details-card" />
           <UserCard className="dashboard-nav-details-card" />
@@ -36,7 +75,7 @@ const Dashboard = () => {
           <UserCard className="dashboard-nav-details-card" />
           <UserCard className="dashboard-nav-details-card" />
           <UserCard className="dashboard-nav-details-card" />
-        </div>
+        </div> */}
       </div>
     </>
   );
