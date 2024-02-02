1ed2f0dc87a6d26e5730d813fbfd2e96e42e64dc
HSangwaiya <ashsangwaiya@gmail.com>
2024-02-02 16:26:54 +0530

    minor-fixes

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,12 +1,32 @@
 import "./dashboard.css";
 import react, { useState } from "react";
 
+import UserCard from "../../components/userCard/UserCard_Registration"
+
 const Dashboard = () => {
+
+
   return (
     <>
       <div className="dashboard">
         <div className="profile_heading">
-            PROFILE
+          PROFILE
+        </div>
+
+        <div className="dashboard-nav">
+          <button className="dashboard-nav-button">REGISTERED EVENTS</button>
+          <button className="dashboard-nav-button">PRONITES</button>
+          <button className="dashboard-nav-button">YOUR ORDERS</button>
+          <button className="dashboard-nav-button">ACCOMODATION</button>
+        </div>
+        <div className="dashboard-nav-details">
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
+          <UserCard className="dashboard-nav-details-card" />
         </div>
       </div>
     </>
