e961e7122991308c875da2aa51fb9d457269d7a6
Rajarshee Das <rajarsheedas14@gmail.com>
2024-02-02 16:25:20 +0530

    Dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,12 +1,21 @@
 import "./dashboard.css";
+import photo from "../Events/poster.webp";
 import react, { useState } from "react";
+import edit_button from "./btn.png";
 
 const Dashboard = () => {
   return (
     <>
       <div className="dashboard">
-        <div className="profile_heading">
-            PROFILE
+        <div className="profile_heading">PROFILE</div>
+        <div className="profile_box">
+          <div className="profile_photo">
+            <img src={photo} alt="" className="userPhoto" />
+            <button>
+              <img src={edit_button} className="edit_photo" alt="" />
+            </button>
+          </div>
+          <div className="profile_details">Rajarshee</div>
         </div>
       </div>
     </>
