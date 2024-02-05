a2cbe2a65daa873ec417be20170c9a0bb65ae61c
HSangwaiya <ashsangwaiya@gmail.com>
2024-02-05 19:19:37 +0530

    events page filter component

--- src/pages/Events/eventmain.jsx
+++ src/pages/Events/eventmain.jsx
@@ -3,6 +3,7 @@ import eventposter from "./eventposter.png";
 import React, { useState } from "react";
 import search_icon from "./search.svg";
 import arrowRight from "./arrow_right.svg";
+import EventCard from "../../components/EventCard/EventCard";
 const EventMain = () => {
   const myStyle = {
     "mask-type": "alpha",
@@ -56,7 +57,7 @@ const EventMain = () => {
         </div>
         <div className="filters">
           <div className="sel_fil_container">
-            <div className="selected_filter">
+            <div className="checkboxed_filter">
               Competitions
               <button>
                 <svg
@@ -91,12 +92,29 @@ const EventMain = () => {
         <div className="event_container">
           <div className="event_sidebar">
             <div className="sidebar_body">
-              <button className="sidebar_button">
+              <button className="sidebar_btn">
+                <img src={arrowRight} alt="" />
+                <div className="sidebar_text">By Type</div>
+              </button>
+              <div className="sidebar_options sidebar_options_hr" id="sidebar_options_type">
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="competitions" />Competitions</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_gl" className="sidebar_option_check" name="guest_lectures" />Guest Lectures</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_ws" className="sidebar_option_check" name="workshops" />Workshops</div>
+              </div>
+              <button className="sidebar_btn">
                 <div className="arrow_sdbar">
                   <img src={arrowRight} alt="" />
                 </div>
-                <div className="sidebar_text">By Type</div>
+                <div className="sidebar_text">By Clubs/Society</div>
               </button>
+              <div className="sidebar_options">
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="aces-acm" />ACES-ACM</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="ches" />Chemical Engineering Society {"("}CHES{")"}</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="mathsoc" />Mathematics Society {"("}MathSoc{")"}</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="cef" />Civil Engineering Forum {"("}CEF{")"}</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="tes" />Textile Engineering Society {"("}TES{")"}</div>
+                <div className="sidebar_option"><input type="checkbox" id="sidebar_option_comp" className="sidebar_option_check" name="mes" />Material Engineering Society {"("}MES{")"}</div>
+              </div>
             </div>
           </div>
           <div className="events">
@@ -112,6 +130,10 @@ const EventMain = () => {
             <div className="event_card">
               <img src={eventposter} alt="" />
             </div>
+            {/* <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard /> */}
           </div>
         </div>
       </div>
