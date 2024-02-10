66f9da01997131f93ac3bdea342a11d942b0be53
harshitmawandia <hmawandia@gmail.com>
2024-02-10 18:52:40 +0530

    updated

--- src/pages/contactus/data.js
+++ src/pages/contactus/data.js
@@ -9,89 +9,90 @@ import events from "../../assets/contact/events.webp";
 import harshit from "../../assets/contact/harshit.webp";
 import shwinder from "../../assets/contact/shwinder.webp";
 
-
 const data = {
-  'ocs': [
-  {
-    name: 'Shwinder Singh',
-    por: 'Overall Coordinator',
-    image: shwinder,
-    email: "caicgsec@iitd.ac.in",
-    linkedin: "https://www.linkedin.com/in/shwinder-singh-0581b71b9/",
-    number: '9711780406'
-  },
-{
-    name: 'Harshit Mawandia',
-    por: 'Co-Overall Coordinator',
-    image: harshit,
-    email: "admin@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/harshit-mawandia/",
-    number: '9031193301'
-  }],
-  'ctms': [{
-    name: 'Sarthak Chauhan',
-    por: 'Marketing',
-    image: marketing,
-    email: "sarthak.chauhan@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/sarthak-singh-chauhan-a1942b209/",
-    number: '6266138056'
-  },
-  {
-    name: 'Rahul Ranveer Shah',
-    por: 'Events',
-    image: events,
-    email: "rahul@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/rahul-ranveer-shah-212075201/",
-    number: '9717326180'
-  },
-  {
-    name: 'Parth Singhal',
-    por: 'Club-Soc',
-    image: club,
-    email: "parth@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/parth-singhal-484568217/",
-    number: '9769410227'
-  },
-  {
-    name: 'Rishika Verma',
-    por: 'Guest Lectures',
-    image: guest,
-    email: "rishika@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/rishika-iitd",
-    number: '9693087393'
-  },
-  {
-    name: 'Arshia Dhiman',
-    por: 'Publicity',
-    image: publi,
-    email: "arshia@tryst-iitd.org",
-    linkedin: "https://www.linkedin.com/in/arshia-dhiman/",
-    number: "984848667"
-  },
-  {
-    name: 'Priyansh Shrivastava',
-    por: 'Technology',
-    image: tech,
-    email: 'priyanshrivastav08@gmail.com',
-    linkedin: "https://www.linkedin.com/in/priyansh-shrivastava-95a89821b/",
-    number: '6264263440'
-  },
-  {
-    name: 'Udit Batish',
-    por: 'Media',
-    image: media,
-    email: "uditbatish@gmail.com",
-    linkedin: "https://www.linkedin.com/in/udit-batish-2753591ba/",
-    number: '9517838866'
-  },
-  {
-    name: 'Arya Ladhani',
-    por: 'Admin & Finance',
-    image: finance,
-    email: "arya.ladhani007@gmail.com",
-    linkedin: "https://www.linkedin.com/in/arya-ladhani-74b621200/",
-    number: '9619952579'
-  },
-  ]
+  ocs: [
+    {
+      name: "Harshit Mawandia",
+      por: "Overall Coordinator",
+      image: harshit,
+      email: "admin@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/harshit-mawandia/",
+      number: "9031193301",
+    },
+    {
+      name: "Shwinder Singh",
+      por: "Overall Coordinator",
+      image: shwinder,
+      email: "caicgsec@iitd.ac.in",
+      linkedin: "https://www.linkedin.com/in/shwinder-singh-0581b71b9/",
+      number: "9711780406",
+    },
+  ],
+  ctms: [
+    {
+      name: "Sarthak Chauhan",
+      por: "Marketing",
+      image: marketing,
+      email: "sarthak.chauhan@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/sarthak-singh-chauhan-a1942b209/",
+      number: "6266138056",
+    },
+    {
+      name: "Rahul Ranveer Shah",
+      por: "Events",
+      image: events,
+      email: "rahul@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/rahul-ranveer-shah-212075201/",
+      number: "9717326180",
+    },
+    {
+      name: "Parth Singhal",
+      por: "Club-Soc",
+      image: club,
+      email: "parth@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/parth-singhal-484568217/",
+      number: "9769410227",
+    },
+    {
+      name: "Rishika Verma",
+      por: "Guest Lectures",
+      image: guest,
+      email: "rishika@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/rishika-iitd",
+      number: "9693087393",
+    },
+    {
+      name: "Arshia Dhiman",
+      por: "Publicity",
+      image: publi,
+      email: "arshia@tryst-iitd.org",
+      linkedin: "https://www.linkedin.com/in/arshia-dhiman/",
+      number: "984848667",
+    },
+    {
+      name: "Priyansh Shrivastava",
+      por: "Technology",
+      image: tech,
+      email: "priyanshrivastav08@gmail.com",
+      linkedin: "https://www.linkedin.com/in/priyansh-shrivastava-95a89821b/",
+      number: "6264263440",
+    },
+    {
+      name: "Udit Batish",
+      por: "Media",
+      image: media,
+      email: "uditbatish@gmail.com",
+      linkedin: "https://www.linkedin.com/in/udit-batish-2753591ba/",
+      number: "9517838866",
+    },
+    {
+      name: "Arya Ladhani",
+      por: "Admin & Finance",
+      image: finance,
+      email: "arya.ladhani007@gmail.com",
+      linkedin: "https://www.linkedin.com/in/arya-ladhani-74b621200/",
+      number: "9619952579",
+    },
+  ],
 };
 export default data;
