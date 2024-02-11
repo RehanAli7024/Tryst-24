6fbd94cdf5690d05168101fd5c6173a99d09ce5b
aryandharwa <bb1221390@iitd.ac.in>
2024-02-12 01:30:29 +0530

    navbar fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,12 +5,15 @@ import Tryst24 from "../../assets/Navbar/TRYST.png";
 import profileicon from "../../assets/Navbar/IconButton.png";
 import navbarmenu from "../../assets/Navbar/navbarmenu.png";
 import crossmenu from "../../assets/Navbar/crossmenu.png";
+import NavEffect from "../../assets/Navbar/nav-mobile-effect.png";
+import NavSelector from "../../assets/Navbar/selector.png";
+import NavMobileBg from "../../assets/Navbar/Mobile Version_Nav Expanded.png";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 function Navbar() {
   const navigate = useNavigate();
   const [showNavOptions, setShowNavOptions] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(""); // Default selected option
+  const [selectedOption, setSelectedOption] = useState(undefined); // Default selected option
   const [selectedMobileOption, setSelectedMobileOption] =
     useState(selectedOption); // Mobile view selected option
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
@@ -41,11 +44,28 @@ function Navbar() {
     };
   }, []);
 
+  useEffect(() => {
+  if (showNavOptions) {
+    document.body.classList.add('no-scroll'); // Add class to disable scrolling
+  } else {
+    document.body.classList.remove('no-scroll'); // Remove class to enable scrolling
+  }
+
+  return () => {
+    document.body.classList.remove('no-scroll'); // Clean up by removing the class when component unmounts
+  };
+}, [showNavOptions]);
+
   return (
     <Container
-      className={`navbar ${isNavbarVisible ? "navbar-visible" : "navbar-hidden"
-        }`}
+      className={`navbar ${
+        isNavbarVisible ? "navbar-visible" : "navbar-hidden"
+      }`}
     >
+        <div className={showNavOptions ? "nav-mobile-bg" : "hidden-nav-mobile-bg"}>
+        <img src={NavMobileBg} alt="" className="nav-effect-back" />
+      </div>
+
       <div className="navbarheader">
         <div
           className="navbartrystlogo"
@@ -57,8 +77,9 @@ function Navbar() {
         <div className="navbaroptions-container">
           <div className="navbaricons">
             <a
-              className={`navbaroption ${selectedOption === "CAP" ? "navbaroption-selected" : ""
-                }`}
+              className={`navbaroption ${
+                selectedOption === "CAP" ? "navbaroption-selected" : ""
+              }`}
               href="https://cap.tryst-iitd.org/"
               target="_blank"
               rel="noreferrer"
@@ -75,8 +96,9 @@ function Navbar() {
             ].map((option) => (
               <div
                 key={option}
-                className={`navbaroption ${selectedOption === option ? "navbaroption-selected" : ""
-                  }`}
+                className={`navbaroption ${
+                  selectedOption === option ? "navbaroption-selected" : ""
+                }`}
                 onClick={() => handleNavbarOptionClick(option)}
               >
                 {option}
@@ -96,6 +118,7 @@ function Navbar() {
       <div
         className={showNavOptions ? "navbariconsmobile" : "hiddenmobiletoggle"}
       >
+
         {[
           "About",
           "Guests",
@@ -106,13 +129,22 @@ function Navbar() {
         ].map((option) => (
           <div
             key={option}
-            className={`navbaroption ${selectedMobileOption === option
+            className={`navbaroption ${
+              selectedMobileOption === option
                 ? "navbaroption-selected-mobile"
                 : ""
-              }`}
+            }`}
             onClick={() => handleNavbarOptionClick(option)}
           >
+            {selectedMobileOption === option && (
+              <img src={NavSelector} alt="" className="nav-effect-front" />
+            )}{" "}
+            {/* Render NavEffect only if the option is selected */}
             {option}
+            {selectedMobileOption === option && (
+              <img src={NavEffect} alt="" />
+            )}{" "}
+            {/* Render NavEffect only if the option is selected */}
           </div>
         ))}
       </div>
