87d9897f51ca69d1dc6b1197c908e4e6c21f5773
RehanAli7024 <bb1221388@iitd.ac.in>
2024-02-22 10:41:09 +0530

    registration accomodation

--- src/pages/accomodation/accomodation.jsx
+++ src/pages/accomodation/accomodation.jsx
@@ -5,7 +5,6 @@ import Placeholder1 from "../contactus/placeholder1";
 import data1 from "./data1";
 import FAQ_main from "./ac_faq_main";
 
-
 const Accomodation = () => {
   const [activeButton, setActiveButton] = useState("Registration Form");
   const [isClicked, setIsClicked] = useState(false);
@@ -68,15 +67,21 @@ const Accomodation = () => {
     });
   };
 
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log("Number of Men:", menCount);
-  console.log("Number of Women:", womenCount);
-  console.log("Check-In Date:", checkInDate);
-  console.log("Check-Out Date:", checkOutDate);
-  console.log("Form submitted with data:", formDataArray);
-  // You can perform further actions like sending the data to a server here
-};
+    e.preventDefault();
+    console.log("Number of Men:", menCount);
+    console.log("Number of Women:", womenCount);
+    console.log("Check-In Date:", checkInDate);
+    console.log("Check-Out Date:", checkOutDate);
+    console.log("Form submitted with data:", formDataArray);
+    // You can perform further actions like sending the data to a server here
+  };
 
   return (
     <>
@@ -135,43 +140,66 @@ const Accomodation = () => {
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">1. </div>
               <div className="ac_reaching_points_text">
-                Travelling in Mumbai is very easy and systematic. The modes of travelling in Mumbai are taxis, auto rickshaws, local train and BEST Buses. IIT Bombay is located at Powai, which is an eastern suburb in the North-Eastern part (Central Railway Line) of Mumbai. The following link may provide you a rough estimate of the auto fares between two stations Taxi Auto fare Mumbai is in the form of a long narrow island, almost a peninsula, thrusting southwards into the Arabian Sea. In Mumbai, local trains run through the following routes. Western Railway: Church gate to Borivali/Virar and return. Central Railway: Mumbai CST to Karjat/Kasara and return. Harbour Route: Mumbai CST to Andheri and return New Bombay Route: Mumbai CST to Vashi/ Panvel and return. Kanjur Marg, a Local Train Station is the closest local train stop to IIT Bombay. It is located on Central Railway line. Kanjur Marg Local Train Station is the closest local train stop to IIT Bombay.
+                Travelling in Mumbai is very easy and systematic. The modes of
+                travelling in Mumbai are taxis, auto rickshaws, local train and
+                BEST Buses. IIT Bombay is located at Powai, which is an eastern
+                suburb in the North-Eastern part (Central Railway Line) of
+                Mumbai. The following link may provide you a rough estimate of
+                the auto fares between two stations Taxi Auto fare Mumbai is in
+                the form of a long narrow island, almost a peninsula, thrusting
+                southwards into the Arabian Sea. In Mumbai, local trains run
+                through the following routes. Western Railway: Church gate to
+                Borivali/Virar and return. Central Railway: Mumbai CST to
+                Karjat/Kasara and return. Harbour Route: Mumbai CST to Andheri
+                and return New Bombay Route: Mumbai CST to Vashi/ Panvel and
+                return. Kanjur Marg, a Local Train Station is the closest local
+                train stop to IIT Bombay. It is located on Central Railway line.
+                Kanjur Marg Local Train Station is the closest local train stop
+                to IIT Bombay.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">2. </div>
               <div className="ac_reaching_points_text">
-                Every team has to register online on the official Techfest website for the competition.
+                Every team has to register online on the official Techfest
+                website for the competition.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">3. </div>
               <div className="ac_reaching_points_text">
-                A Team ID will be allocated to the team on registration which shall be used for future references.
+                A Team ID will be allocated to the team on registration which
+                shall be used for future references.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">4. </div>
               <div className="ac_reaching_points_text">
-                The decision of the organizers or judges shall be treated as final and binding on all.
+                The decision of the organizers or judges shall be treated as
+                final and binding on all.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">5. </div>
               <div className="ac_reaching_points_text">
-                No responsibility will be held by Techfest, IIT Bombay for any late, lost or misdirected entries.
+                No responsibility will be held by Techfest, IIT Bombay for any
+                late, lost or misdirected entries.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">6. </div>
               <div className="ac_reaching_points_text">
-                Note that at any point of time the latest information will be that which is on the website. However, registered participants will be informed through mail about any changes.
+                Note that at any point of time the latest information will be
+                that which is on the website. However, registered participants
+                will be informed through mail about any changes.
               </div>
             </div>
             <div className="ac_reaching_points">
               <div className="ac_reaching_points_num">7. </div>
               <div className="ac_reaching_points_text">
-                All modes of official communication will be through the Techfest e-mail. Participants are advised to keep track of all folders in their e-mail accounts.
+                All modes of official communication will be through the Techfest
+                e-mail. Participants are advised to keep track of all folders in
+                their e-mail accounts.
               </div>
             </div>
           </div>
@@ -357,7 +385,24 @@ const Accomodation = () => {
                     </form>
                   </div>
                 ))}
-                <button className="submit_details_members" onClick={handleSubmit}>Submit</button>
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={isChecked}
+                      onChange={handleCheckboxChange}
+                    />
+                    I have read all terms and conditions
+                  </label>
+                </div>
+                {isChecked && (
+                  <button
+                    className="submit_details_members"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                )}
               </div>
             )}
           </div>
