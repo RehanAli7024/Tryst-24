deb9baf4a4bdd4eaded719e81cd6ce8ae2d38d7b
HSangwaiya <ashsangwaiya@gmail.com>
2024-02-24 04:19:22 +0530

    speaker card fixes

--- src/components/SpeakerCard/SpeakerCard.jsx
+++ src/components/SpeakerCard/SpeakerCard.jsx
@@ -1,22 +1,21 @@
 import "./SpeakerCard.css"
-import TestImage from "../../assets/event_cards/ashneer.png"
 import Border from "../../assets/event_cards/speaker-border.png"
 
-const SpeakerCard = () => {
+function SpeakerCard({ speaker }) {
     return (
         <div className="speaker-card-container speaker-shape">
             <div className="speaker-card speaker-shape">
-                <div className="speaker-card-name">Ashneer Grover</div>
+                <div className="speaker-card-name">{speaker.name}</div>
                 <div className="speaker-card-image-container speaker-shape-2">
                     <div className="speaker-card-image speaker-shape-2">
                         <img src={Border} alt="Border" className="speaker-card-border" />
                         <div className="speaker-card-position-container speaker-shape-3">
                             <div className="speaker-card-position speaker-shape-3">
-                                <div className="speaker-card-position-top">Founder</div>
-                                <div className="speaker-card-position-bottom">BharatPe</div>
+                                <div className="speaker-card-position-top">{speaker.position}</div>
+                                <div className="speaker-card-position-bottom">{speaker.company}</div>
                             </div>
                         </div>
-                        <img src={TestImage} alt="Ashneer Grover" className="speaker-card-img" />
+                        <img src={speaker.image} alt="Ashneer Grover" className="speaker-card-img" />
                     </div>
                 </div>
             </div>
--- src/pages/landing-page/components/landing-speakers.jsx
+++ src/pages/landing-page/components/landing-speakers.jsx
@@ -1,8 +1,54 @@
 import "./landing-speakers.css"
 import SpeakerCard from "../../../components/SpeakerCard/SpeakerCard.jsx"
+import TestImage from "../../../assets/event_cards/ashneer.png"
+
+const speakers = [
+    {
+        index: 1,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+    {
+        index: 2,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+    {
+        index: 3,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+    {
+        index: 4,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+    {
+        index: 5,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+    {
+        index: 6,
+        name: "Ashneer Grover",
+        position: "Founder",
+        company: "BharatPe",
+        image: TestImage,
+    },
+]
 
 const Speakers = () => {
-    
+
     return (
         <div className="landing-comp">
             <div className="landing-heading">
@@ -10,12 +56,9 @@ const Speakers = () => {
             </div>
             <div className="landing-speakers-container">
                 <div className="landing-speakers-cards">
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
-                    <div className="landing-speaker-card"><SpeakerCard /></div>
+                    {speakers.map(speaker => (
+                        <div className="landing-speaker-card"><SpeakerCard speaker={speaker} /></div>
+                    ))}
                 </div>
             </div>
         </div>
