621324163e097eea1cafee522116462bb06ca0e4
HSangwaiya <ashsangwaiya@gmail.com>
2024-02-24 19:05:44 +0530

    fix

--- src/pages/landing-page/spline.jsx
+++ src/pages/landing-page/spline.jsx
@@ -6,8 +6,6 @@ const SplineDesign = () => {
       <iframe
         title="Spline Design"
         src="https://my.spline.design/untitled-5d41a3b910da886b517de3041827597a/"
-        width="100%"
-        height="500px"
         frameBorder="0"
       />
     </div>
