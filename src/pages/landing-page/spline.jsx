c9c5e673fb7657c8fad5cfe21f748993147f85b7
HSangwaiya <ashsangwaiya@gmail.com>
2024-02-24 18:54:06 +0530

    fix

--- src/pages/landing-page/spline.jsx
+++ src/pages/landing-page/spline.jsx
@@ -7,7 +7,7 @@ const SplineDesign = () => {
         title="Spline Design"
         src="https://my.spline.design/untitled-5d41a3b910da886b517de3041827597a/"
         width="100%"
-        height="700px"
+        height="500px"
         frameBorder="0"
       />
     </div>
