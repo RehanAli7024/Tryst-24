233da2f90448599287978a3108dbf16b9eeb4bd6
lucky-389 <girvarpatidar8955@gmail.com>
2024-03-02 03:24:33 +0530

    fix

--- src/pages/sponsors/sponsors.jsx
+++ src/pages/sponsors/sponsors.jsx
@@ -1,5 +1,5 @@
 import './sponsors.css';
-import Adobe from "../../assets/sponsors/adobe.webp";
+import Samsung from "../../assets/sponsors/samsung.webp";
 import TechMaghi from "../../assets/sponsors/tm.webp";
 import RemarkSkill from "../../assets/sponsors/remarkskill.webp";
 import Robocap from "../../assets/sponsors/robocap.webp";
@@ -18,13 +18,13 @@ import SponsorCard from '../../components/SponsorCard/SponsorCard';
 const sponsors = [
     {
         index: 1,
-        name: "TechMaghi",
+        name: "Nucleus Software",
         image: TechMaghi,
         // header: "WORKSHOP PARTNER",
     },
     {
         index: 2,
-        name: "Remarkskill",
+        name: "Nucleus Software",
         image: RemarkSkill,
         header: "WORKSHOP PARTNER",
     },
@@ -74,6 +74,111 @@ const sponsors = [
         name: "SiliconIndia",
         image: SiliconIndia,
     },
+    {
+        index: 12,
+        name: "TechMaghi",
+        image: TechMaghi,
+    },
+    {
+        index: 13,
+        name: "Remarkskill",
+        image: RemarkSkill,
+    },
+    {
+        index: 14,
+        name: "Robocap League",
+        image: Robocap,
+    },
+    {
+        index: 15,
+        name: "Edutech Life",
+        image: Edutech,
+    },
+    {
+        index: 16,
+        name: "Techobyte",
+        image: Techobyte,
+    },
+    {
+        index: 17,
+        name: "Robosapiens",
+        image: Robosapiens,
+    },
+    {
+        index: 18,
+        name: "Wingfotech",
+        image: Wingfotech,
+    },
+    {
+        index: 19,
+        name: "Orkes",
+        image: Orkes,
+    },
+    {
+        index: 20,
+        name: "Jagran Josh",
+        image: Jagran,
+    },
+    {
+        index: 21,
+        name: "UnStop",
+        image: UnStop,
+    },
+    {
+        index: 22,
+        name: "SiliconIndia",
+        image: SiliconIndia,
+    },
+    {
+        index: 23,
+        name: "TechMaghi",
+        image: TechMaghi,
+    },
+    {
+        index: 24,
+        name: "Remarkskill",
+        image: RemarkSkill,
+    },
+    {
+        index: 25,
+        name: "Robocap League",
+        image: Robocap,
+    },
+    {
+        index: 26,
+        name: "Edutech Life",
+        image: Edutech,
+    },
+    {
+        index: 27,
+        name: "Techobyte",
+        image: Techobyte,
+    },
+    {
+        index: 28,
+        name: "Robosapiens",
+        image: Robosapiens,
+    },
+    {
+        index: 29,
+        name: "Wingfotech",
+        image: Wingfotech,
+    },
+    {
+        index: 30,
+        name: "Orkes",
+        image: Orkes,
+    },
+    {
+        index: 31,
+        name: "Jagran Josh",
+        image: Jagran,
+    },
+    {
+        index: 32,
+        name: "UnStop",
+        image: UnStop,
+    },
 ]
 
 
@@ -86,10 +191,11 @@ function Sponsors() {
                     <div className="sponsors-title-sponsor sponsors-shape-title">
                         <div className="sponsors-title-sponsor-subcontainer sponsors-shape-title">
                             <div className="sponsors-title-sponsor-img-div">
-                                <img src={Adobe} alt="Adobe Inc." className='sponsors-title-sponsor-img' />
+                                <img src={Samsung} alt="Samsung" className='sponsors-title-sponsor-img' />
                             </div>
-                            <div className="sponsors-title-sponsor-text">Adobe Inc.</div></div>
+                            <div className="sponsors-title-sponsor-text">Samsung</div></div>
                     </div>
+                    {/* <div className="sponsors-other-sponsors-type">WORKSHOP PARTNER</div> */}
                     <div className='sponsors-other-sponsors'>
                         {sponsors.map(sponsor => (
                             <div key={sponsor.index} className="sponsors-other-sponsors-container" id={`sponsor${sponsor.index}`}>
@@ -106,6 +212,23 @@ function Sponsors() {
                             // <SponsorCard sponsor={sponsor} />
                         ))}
                     </div>
+                    {/* <div className="sponsors-other-sponsors-type"></div>
+                    <div className='sponsors-other-sponsors'>
+                        {sponsors.map(sponsor => (
+                            <div key={sponsor.index} className="sponsors-other-sponsors-container" id={`sponsor${sponsor.index}`}>
+                                <div className="sponsors-other-sponsors-head">{sponsor.header}</div>
+                                <div className="sponsors-other-sponsors-box-container sponsors-shape">
+                                    <div className="sponsors-other-sponsors-box sponsors-shape">
+                                        <div className="sponsors-other-sponsors-box-img-div">
+                                            <img src={sponsor.image} alt={sponsor.name} className="sponsors-other-sponsors-box-img" />
+                                        </div>
+                                        <div className="sponsors-other-sponsors-box-text">{sponsor.name}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            // <SponsorCard sponsor={sponsor} />
+                        ))}
+                    </div> */}
                 </div>
             </div >
         </>
