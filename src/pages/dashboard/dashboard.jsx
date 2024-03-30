d739145a19b542edb8f6b895877b62868b8c5477
Ankit Meena <124269099+akmeena5726@users.noreply.github.com>
2024-03-31 00:25:02 +0530

    fixes download pass button

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -164,7 +164,7 @@ const Dashboard = () => {
       })
       .catch((error) => {
         console.log(error);
-        alert("You have not registered for Technite 1")
+        alert("You have not registered for Technite 1");
         setPassDownloadStatus(false);
       });
   };
@@ -174,7 +174,7 @@ const Dashboard = () => {
       const qrUrl = await QRCode.toDataURL(text);
       return qrUrl;
     } catch (err) {
-      console.error('Error generating QR code', err);
+      console.error("Error generating QR code", err);
     }
   };
   return (
@@ -275,29 +275,33 @@ const Dashboard = () => {
 
         <div className="dashboard-nav">
           <button
-            className={`dashboard-nav-button ${activeButton === "REGISTERED EVENTS" ? "active" : ""
-              }`}
+            className={`dashboard-nav-button ${
+              activeButton === "REGISTERED EVENTS" ? "active" : ""
+            }`}
             onClick={() => handleButtonClick("REGISTERED EVENTS")}
           >
             REGISTERED EVENTS
           </button>
           <button
-            className={`dashboard-nav-button ${activeButton === "TECHNITES" ? "active" : ""
-              }`}
+            className={`dashboard-nav-button ${
+              activeButton === "TECHNITES" ? "active" : ""
+            }`}
             onClick={() => handleButtonClick("TECHNITES")}
           >
             TECHNITES
           </button>
           <button
-            className={`dashboard-nav-button ${activeButton === "YOUR ORDERS" ? "active" : ""
-              }`}
+            className={`dashboard-nav-button ${
+              activeButton === "YOUR ORDERS" ? "active" : ""
+            }`}
             onClick={() => handleButtonClick("YOUR ORDERS")}
           >
             YOUR ORDERS
           </button>
           <button
-            className={`dashboard-nav-button ${activeButton === "ACCOMODATION" ? "active" : ""
-              }`}
+            className={`dashboard-nav-button ${
+              activeButton === "ACCOMODATION" ? "active" : ""
+            }`}
             onClick={() => handleButtonClick("ACCOMODATION")}
           >
             ACCOMODATION
@@ -316,7 +320,11 @@ const Dashboard = () => {
         )}
         {activeButton === "TECHNITES" && (
           <div>
-            <button className="pronite-card" onClick={handlepassdownload} disabled={passDownloadStatus}>
+            <button
+              className="pass-download-button"
+              onClick={handlepassdownload}
+              disabled={passDownloadStatus}
+            >
               Download Technite 1 Pass
             </button>
             {pdfData && (
@@ -324,9 +332,9 @@ const Dashboard = () => {
                 {({ blob, url, loading, error }) => {
                   if (url) {
                     // Create a temporary link to trigger the download
-                    const downloadLink = document.createElement('a');
+                    const downloadLink = document.createElement("a");
                     downloadLink.href = url;
-                    downloadLink.download = 'Pass.pdf'; // The file name of the downloaded PDF
+                    downloadLink.download = "Pass.pdf"; // The file name of the downloaded PDF
                     document.body.appendChild(downloadLink);
                     downloadLink.click();
                     document.body.removeChild(downloadLink);
